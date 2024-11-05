26e7d42f139035a20100f132f860d0ae139cc7fa
Colby Wang <colbyziyuwang@gmail.com>
2024-11-04 22:35:13 -0500

    loss function, example using graphsage

--- graphsage/model.py
+++ graphsage/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import init
+import torch.nn.functional as F
 from torch.autograd import Variable
 
 import numpy as np
@@ -21,184 +22,64 @@ import pickle
 import pandas as pd
 
 """
-Simple supervised GraphSAGE model as well as examples running the model
-on the Cora and Pubmed datasets.
+Simple supervised GraphSAGE model to learn useful node embeddings for user
+and recipes
 """
 
 class SupervisedGraphSage(nn.Module):
-    def __init__(self, num_classes, enc, user_dim=25076, recipe_dim=768):
+    def __init__(self, enc, user_project_layer, recipe_project_layer, 
+                 user_dim=25076, recipe_dim=768, common_dim=512):
         super(SupervisedGraphSage, self).__init__()
         self.enc = enc
-        self.xent = nn.CrossEntropyLoss()
-
-        self.weight = nn.Parameter(torch.FloatTensor(num_classes, enc.embed_dim))
-        init.xavier_uniform_(self.weight)
 
         self.user_dim = user_dim
         self.recipe_dim = recipe_dim
-        self.common_dim = 512
+        self.common_dim = common_dim
 
         # Projection layers
-        self.user_project = nn.Linear(user_dim, self.common_dim)  # projects user embedding to common_dim
-        self.recipe_project = nn.Linear(recipe_dim, self.common_dim)  # projects recipe embedding to common_dim
+        self.user_project = user_project_layer  # projects user embedding to common_dim
+        self.recipe_project = recipe_project_layer  # projects recipe embedding to common_dim
 
-    def forward(self, nodes):
-        embeds = self.enc(nodes)
-        scores = self.weight.mm(embeds)
-        return scores.t()
+        # Loss Parameters
+        self.alpha_n=1.0
+        self.alpha_l=0.5
+        self.delta_n=1.0
+        self.delta_l=0.5
 
-    def loss(self, nodes, labels):
-        scores = self.forward(nodes)
-        return self.xent(scores, labels.squeeze())
+    def max_margin_loss(self, user_embedding, pos_embedding, neg_embedding, low_rank_embedding):
+        """
+        Parameters:
+        - user_embedding: User embedding (batch of users).
+        - pos_embedding: Embedding of high-rated (positive) recipes (4-5).
+        - neg_embedding: Embedding of non-interacted (negative) recipes.
+        - low_rank_embedding: Embedding of low-rated (low-rank positive) recipes (1-3).
+        - alpha_n: Weight for negative term in loss.
+        - alpha_l: Weight for low-rank positive term in loss.
+        - delta_n: Margin for negative term.
+        - delta_l: Margin for low-rank positive term.
 
-def load_cora():
-    num_nodes = 2708
-    num_feats = 1433
-    feat_data = np.zeros((num_nodes, num_feats))
-    labels = np.empty((num_nodes,1), dtype=np.int64)
-    node_map = {}
-    label_map = {}
-    with open("cora/cora.content") as fp:
-        for i,line in enumerate(fp):
-            info = line.strip().split()
-            feat_data[i,:] = list(map(float, info[1:-1]))
-            node_map[info[0]] = i
-            if not info[-1] in label_map:
-                label_map[info[-1]] = len(label_map)
-            labels[i] = label_map[info[-1]]
+        Returns:
+        - Max-margin loss with low-rank positive augmentation.
+        """
+        # Compute positive interaction scores
+        pos_score = torch.sum(user_embedding * pos_embedding, dim=1)
 
-    adj_lists = defaultdict(set)
-    with open("cora/cora.cites") as fp:
-        for i,line in enumerate(fp):
-            info = line.strip().split()
-            paper1 = node_map[info[0]]
-            paper2 = node_map[info[1]]
-            adj_lists[paper1].add(paper2)
-            adj_lists[paper2].add(paper1)
-    return feat_data, labels, adj_lists
+        # Compute negative interaction scores
+        neg_score = torch.sum(user_embedding * neg_embedding, dim=1)
 
-def run_cora():
-    np.random.seed(1)
-    random.seed(1)
-    num_nodes = 2708
-    feat_data, labels, adj_lists = load_cora()
-    features = nn.Embedding(2708, 1433)
-    features.weight = nn.Parameter(torch.FloatTensor(feat_data), requires_grad=False)
-   # features.cuda()
+        # Compute low-rank positive interaction scores
+        low_rank_score = torch.sum(user_embedding * low_rank_embedding, dim=1)
 
-    agg1 = MeanAggregator(features, cuda=True)
-    enc1 = Encoder(features, 1433, 128, adj_lists, agg1, gcn=True, cuda=False)
-    agg2 = MeanAggregator(lambda nodes : enc1(nodes).t(), cuda=False)
-    enc2 = Encoder(lambda nodes : enc1(nodes).t(), enc1.embed_dim, 128, adj_lists, agg2,
-            base_model=enc1, gcn=True, cuda=False)
-    enc1.num_samples = 5
-    enc2.num_samples = 5
+        # Max-margin loss for negatives
+        neg_loss = F.relu(-pos_score + neg_score + self.delta_n)
 
-    graphsage = SupervisedGraphSage(7, enc2)
-#    graphsage.cuda()
-    rand_indices = np.random.permutation(num_nodes)
-    test = rand_indices[:1000]
-    val = rand_indices[1000:1500]
-    train = list(rand_indices[1500:])
+        # Max-margin loss for low-rank positives
+        low_rank_loss = F.relu(-pos_score + low_rank_score + self.delta_l)
 
-    optimizer = torch.optim.SGD(filter(lambda p : p.requires_grad, graphsage.parameters()), lr=0.7)
-    times = []
-    for batch in tqdm(range(100), desc="train"):
-        batch_nodes = train[:256]
-        random.shuffle(train)
-        start_time = time.time()
-        optimizer.zero_grad()
-        loss = graphsage.loss(batch_nodes, 
-                Variable(torch.LongTensor(labels[np.array(batch_nodes)])))
-        loss.backward()
-        optimizer.step()
-        end_time = time.time()
-        times.append(end_time-start_time)
-        print(batch, loss.item())
+        # Total loss with weighting
+        loss = self.alpha_n * neg_loss + self.alpha_l * low_rank_loss
 
-    val_output = graphsage.forward(val) 
-    print("Validation F1:", f1_score(labels[val], val_output.data.numpy().argmax(axis=1), average="micro"))
-    print("Average batch time:", np.mean(times))
-
-    # Save the model
-    torch.save(graphsage.state_dict(), "graphsage_model.pth")
-    print("saved model")
-
-def load_pubmed():
-    #hardcoded for simplicity...
-    num_nodes = 19717
-    num_feats = 500
-    feat_data = np.zeros((num_nodes, num_feats))
-    labels = np.empty((num_nodes, 1), dtype=np.int64)
-    node_map = {}
-    with open("pubmed-data/Pubmed-Diabetes.NODE.paper.tab") as fp:
-        fp.readline()
-        feat_map = {entry.split(":")[1]:i-1 for i,entry in enumerate(fp.readline().split("\t"))}
-        for i, line in enumerate(fp):
-            info = line.split("\t")
-            node_map[info[0]] = i
-            labels[i] = int(info[1].split("=")[1])-1
-            for word_info in info[2:-1]:
-                word_info = word_info.split("=")
-                feat_data[i][feat_map[word_info[0]]] = float(word_info[1])
-    adj_lists = defaultdict(set)
-    with open("pubmed-data/Pubmed-Diabetes.DIRECTED.cites.tab") as fp:
-        fp.readline()
-        fp.readline()
-        for line in fp:
-            info = line.strip().split("\t")
-            paper1 = node_map[info[1].split(":")[1]]
-            paper2 = node_map[info[-1].split(":")[1]]
-            adj_lists[paper1].add(paper2)
-            adj_lists[paper2].add(paper1)
-    return feat_data, labels, adj_lists
-
-def run_pubmed():
-    np.random.seed(1)
-    random.seed(1)
-    num_nodes = 19717
-    feat_data, labels, adj_lists = load_pubmed()
-    features = nn.Embedding(19717, 500)
-    features.weight = nn.Parameter(torch.FloatTensor(feat_data), requires_grad=False)
-   # features.cuda()
-
-    agg1 = MeanAggregator(features, cuda=True)
-    enc1 = Encoder(features, 500, 128, adj_lists, agg1, gcn=True, cuda=False)
-    agg2 = MeanAggregator(lambda nodes : enc1(nodes).t(), cuda=False)
-    enc2 = Encoder(lambda nodes : enc1(nodes).t(), enc1.embed_dim, 128, adj_lists, agg2,
-            base_model=enc1, gcn=True, cuda=False)
-    enc1.num_samples = 10
-    enc2.num_samples = 25
-
-    graphsage = SupervisedGraphSage(3, enc2)
-#    graphsage.cuda()
-    rand_indices = np.random.permutation(num_nodes)
-    test = rand_indices[:1000]
-    val = rand_indices[1000:1500]
-    train = list(rand_indices[1500:])
-
-    optimizer = torch.optim.SGD(filter(lambda p : p.requires_grad, graphsage.parameters()), lr=0.7)
-    times = []
-    for batch in tqdm(range(200), desc="train"):
-        batch_nodes = train[:1024]
-        random.shuffle(train)
-        start_time = time.time()
-        optimizer.zero_grad()
-        loss = graphsage.loss(batch_nodes, 
-                Variable(torch.LongTensor(labels[np.array(batch_nodes)])))
-        loss.backward()
-        optimizer.step()
-        end_time = time.time()
-        times.append(end_time-start_time)
-        print(batch, loss.item())
-
-    val_output = graphsage.forward(val) 
-    print("Validation F1:", f1_score(labels[val], val_output.data.numpy().argmax(axis=1), average="micro"))
-    print("Average batch time:", np.mean(times))
-
-    # Save the model
-    torch.save(graphsage.state_dict(), "graphsage_model.pth")
-    print("saved model")
+        return loss.mean()
 
 def create_food_embedding():
     dataset_path = "food-data/"
